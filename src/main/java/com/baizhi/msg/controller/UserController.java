894abf288159d5aa0fb435daae474fecdf60a32a
skare <2379598522@qq.com>
2018-11-26 20:40:55 +0800

    add aop into spring boot

--- src/main/java/com/baizhi/msg/controller/UserController.java
+++ src/main/java/com/baizhi/msg/controller/UserController.java
@@ -17,10 +17,10 @@ public class UserController {
     @RequestMapping("queryAll")
     public String queryAll(String name, Map map) {
         List<User> list = userService.queryAll(name);
-        
-        
         map.put("userlist", list);
         System.out.println("黄河,黄河,我是长江");
+        System.out.println("黄河");
+        System.out.println("长江,长江,我是黄河");
         return "index";
     }
 }
